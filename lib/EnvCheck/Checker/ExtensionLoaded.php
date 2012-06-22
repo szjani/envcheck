a964c73fc3d0f8646113b9be8ddabba4ed64b15b
Szurovecz János <szjani@szjani.hu>
2012-06-23 00:36:36 +0200

    extension checker

---
+++ lib/EnvCheck/Checker/ExtensionLoaded.php
@@ -0,0 +1,43 @@
+<?php
+namespace EnvCheck\Checker;
+use EnvCheck\AbstractChecker;
+use EnvCheck\Result;
+
+/**
+ * Check that a PHP extension has been loaded or not.
+ *
+ * @category    EnvCheck
+ * @package     Checker
+ * @license     http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @author      Szurovecz János <szjani@szjani.hu>
+ */
+class ExtensionLoaded extends AbstractChecker {
+  
+  protected $extensionName;
+  
+  /**
+   * @param string $extensionName
+   * @param int $priority 
+   */
+  public function __construct($extensionName, $priority = 1) {
+    parent::__construct($priority);
+    $this->extensionName = $extensionName;
+  }
+  
+  /**
+   * @return boolean
+   */
+  protected function isLoaded() {
+    return extension_loaded($this->extensionName);
+  }
+  
+  /**
+   * @return \EnvCheck\Result 
+   */
+  protected function currentCheck() {
+    return $this->createResult(
+      sprintf("Whether PHP extension '%s' has been loaded", $this->extensionName),
+      $this->isLoaded() 
+   );
+  }
+}
