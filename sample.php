15dd05d77eab895a8bc192e341c0a5221e24a1fd
Szurovecz János <szjani@szjani.hu>
2012-06-23 13:33:15 +0200

    use fluent interface

--- sample.php
+++ sample.php
@@ -5,8 +5,9 @@ use EnvCheck\Checker;
 use EnvCheck\Observer\CliWriter;
 
 $composite = new Checker\Composite(1);
-$composite->add(new Checker\PhpVersion('5.4', '>'));
-$composite->add(new Checker\PhpExtensionLoaded('pdo_mysql'));
-$composite->add(new Checker\FileMode(new SplFileInfo(__FILE__), Checker\FileMode::ALL));
-$composite->addObserver(new CliWriter());
-$composite->check();
+$composite
+  ->add(new Checker\PhpVersion('5.4', '>'))
+  ->add(new Checker\PhpExtensionLoaded('pdo_mysql'))
+  ->add(new Checker\FileMode(new SplFileInfo(__FILE__), Checker\FileMode::ALL))
+  ->addObserver(new CliWriter())
+  ->check();
