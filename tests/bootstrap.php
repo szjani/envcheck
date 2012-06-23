1a99ac4435f3a0e826a6c6361132593e141b1db2
Szurovecz János <szjani@szjani.hu>
2012-06-23 14:37:59 +0200

    autoload ZF classes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,17 @@
 <?php
 error_reporting(E_ALL|E_STRICT|E_DEPRECATED);
 
+set_include_path(implode(PATH_SEPARATOR, array(
+  get_include_path(),
+  '/development/Frameworks/ZF_1.11_svn/library'
+)));
+
 spl_autoload_register(function($className) {
-  $relativePath = str_replace('\\', '/', $className);
+  $relativePath = str_replace(array('\\', '_'), '/', $className);
   $relativePath = trim($relativePath, '/') . '.php';
   if (file_exists(__DIR__ . '/../lib/' . $relativePath)) {
     require_once __DIR__ . '/../lib/' . $relativePath;
+  } elseif (strpos($className, 'Zend') === 0) {
+    require_once $relativePath;
   }
 });
