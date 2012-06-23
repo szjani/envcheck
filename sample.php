eb1878636dc731ae68748eeea5fcd40d4256dc1e
Szurovecz János <szjani@szjani.hu>
2012-06-23 13:30:15 +0200

    file mode checker sample

--- sample.php
+++ sample.php
@@ -7,5 +7,6 @@ use EnvCheck\Observer\CliWriter;
 $composite = new Checker\Composite(1);
 $composite->add(new Checker\PhpVersion('5.4', '>'));
 $composite->add(new Checker\PhpExtensionLoaded('pdo_mysql'));
+$composite->add(new Checker\FileMode(new SplFileInfo(__FILE__), Checker\FileMode::ALL));
 $composite->addObserver(new CliWriter());
 $composite->check();
